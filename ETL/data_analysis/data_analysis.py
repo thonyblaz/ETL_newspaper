dcc8d18c208b4050a05fab4254dd9584860295d7
Anthony  Blaz <thonyblaz@hotmail.com>
2022-03-08 20:52:45 -0400

    Nuevas columnas para la transformacion de datos

--- ETL/data_analysis/data_analysis.py
+++ ETL/data_analysis/data_analysis.py
@@ -13,12 +13,22 @@ def data_wrangling(df_data):
     df_data['host']=df_data['url'].apply(lambda url: urlparse(url).netloc)
     #eliminiar filas con datos faltantes
     df_data=df_data.dropna(subset=["contents","title"])
+    #Para eliminar filas repetidas
+    df_data.drop_duplicates(subset=['title'],keep='first',inplace=True)
+    #Convertir a minusculas
+    df_data['contents']=df_data.apply(lambda row: row['contents'].lower(), axis=1)
+    df_data['title']=df_data.apply(lambda row: row['title'].lower(), axis=1)
     #crear un hash para cada elemento
     uids=(df_data
         .apply(lambda row:hashlib.md5(bytes(row['url'].encode())),axis=1)
         .apply(lambda hash_object: hash_object.hexdigest())
       )
     df_data['uid']=uids
+    #vemos la longitud de cada titulo
+    leng_title = df_data.apply(lambda row: len(row['title']), axis=1)
+    leng_contents = df_data.apply(lambda row: len(row['contents']), axis=1)
+    df_data['leng title']=leng_title
+    df_data['leng contents']=leng_contents
     #devolvemos la data
     print(df_data)
 
